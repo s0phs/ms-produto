78cb3ca869995ff0549465104809c358aa20b35a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-13 08:23:56 -0300

    first commit

---
+++ src/main/java/com/github/s0phs/ms/produto/MsProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.github.s0phs.ms.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/s0phs/ms/produto/MsProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.s0phs.ms.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
